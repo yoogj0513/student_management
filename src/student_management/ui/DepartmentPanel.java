2b3bf137303fd085e40fd9c70bea6f9970e2a4b0
yoogj0513 <yoogj0513@naver.com>
2020-01-10 15:04:03 +0900

    2020/01/10 학생관리 프로그램 프로젝트 - 클래스(Student, Department), 패널(StudentPanel, DepartmentPanel) 생성

---
+++ src/student_management/ui/DepartmentPanel.java
@@ -0,0 +1,78 @@
+package student_management.ui;
+
+import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
+
+import student_management.dto.Department;
+
+import javax.swing.UIManager;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.GridLayout;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+public class DepartmentPanel extends JPanel {
+	private JLabel lblDeptNo;
+	private JTextField tfDeptNo;
+	private JLabel lblDeptName;
+	private JTextField tfDeptName;
+	private JLabel lblFloor;
+	private JTextField tfFloor;
+
+	/**
+	 * Create the panel.
+	 */
+	public DepartmentPanel() {
+
+		initialize();
+	}
+	private void initialize() {
+		setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "\uD559\uACFC \uC815\uBCF4", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		setLayout(new GridLayout(0, 2, 10, 10));
+		
+		lblDeptNo = new JLabel("학과번호");
+		lblDeptNo.setHorizontalAlignment(SwingConstants.RIGHT);
+		add(lblDeptNo);
+		
+		tfDeptNo = new JTextField();
+		add(tfDeptNo);
+		tfDeptNo.setColumns(10);
+		
+		lblDeptName = new JLabel("학과명");
+		lblDeptName.setHorizontalAlignment(SwingConstants.RIGHT);
+		add(lblDeptName);
+		
+		tfDeptName = new JTextField();
+		tfDeptName.setColumns(10);
+		add(tfDeptName);
+		
+		lblFloor = new JLabel("위치");
+		lblFloor.setHorizontalAlignment(SwingConstants.RIGHT);
+		add(lblFloor);
+		
+		tfFloor = new JTextField();
+		tfFloor.setColumns(10);
+		add(tfFloor);
+	}
+	
+	public Department getDepartment() {
+		int deptNo = Integer.parseInt(tfDeptNo.getText().trim());
+		String deptName= tfDeptName.getText().trim();
+		int floor = Integer.parseInt(tfFloor.getText().trim());
+		Department dept = new Department(deptNo, deptName, floor);
+		return dept;
+	}
+	
+	public void setDepartment(Department dept) {
+		tfDeptNo.setText(dept.getDeptNo()+"");
+		tfDeptName.setText(dept.getDeptName());
+		tfFloor.setText(dept.getFloor()+"");
+	}
+	
+	public void clearTf() {
+		tfDeptNo.setText("");
+		tfDeptName.setText("");
+		tfFloor.setText("");
+	}
+}
---
+++ src/student_management/ui/StudentPanel.java
@@ -0,0 +1,102 @@
+package student_management.ui;
+
+import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
+
+import student_management.dto.Student;
+
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+public class StudentPanel extends JPanel {
+	private JLabel lblStdNo;
+	private JTextField tfStdNo;
+	private JLabel lblStdName;
+	private JTextField tfStdName;
+	private JLabel lblKor;
+	private JTextField tfKor;
+	private JLabel lblEng;
+	private JTextField tfMath;
+	private JLabel lblMath;
+	private JTextField tfEng;
+
+	/**
+	 * Create the panel.
+	 */
+	public StudentPanel() {
+
+		initialize();
+	}
+	private void initialize() {
+		setBorder(new TitledBorder(null, "\uD559\uC0DD\uC815\uBCF4", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		setLayout(new GridLayout(0, 2, 10, 10));
+		
+		lblStdNo = new JLabel("학생번호");
+		lblStdNo.setHorizontalAlignment(SwingConstants.RIGHT);
+		add(lblStdNo);
+		
+		tfStdNo = new JTextField();
+		add(tfStdNo);
+		tfStdNo.setColumns(10);
+		
+		lblStdName = new JLabel("학생이름");
+		lblStdName.setHorizontalAlignment(SwingConstants.RIGHT);
+		add(lblStdName);
+		
+		tfStdName = new JTextField();
+		add(tfStdName);
+		tfStdName.setColumns(10);
+		
+		lblKor = new JLabel("국어");
+		lblKor.setHorizontalAlignment(SwingConstants.RIGHT);
+		add(lblKor);
+		
+		tfKor = new JTextField();
+		add(tfKor);
+		tfKor.setColumns(10);
+		
+		lblEng = new JLabel("수학");
+		lblEng.setHorizontalAlignment(SwingConstants.RIGHT);
+		add(lblEng);
+		
+		tfMath = new JTextField();
+		add(tfMath);
+		tfMath.setColumns(10);
+		
+		lblMath = new JLabel("영어");
+		lblMath.setHorizontalAlignment(SwingConstants.RIGHT);
+		add(lblMath);
+		
+		tfEng = new JTextField();
+		add(tfEng);
+		tfEng.setColumns(10);
+	}
+	
+	public Student getStudent() {
+		int stdNo = Integer.parseInt(tfStdNo.getText().trim());
+		String stdName = tfStdName.getText().trim();
+		int kor = Integer.parseInt(tfKor.getText().trim());
+		int math = Integer.parseInt(tfMath.getText().trim());
+		int eng = Integer.parseInt(tfEng.getText().trim());
+		Student std = new Student(stdNo, stdName, kor, math, eng);
+		return std;
+	}
+	
+	public void setStudent(Student std) {
+		tfStdNo.setText(std.getStdNo()+"");
+		tfStdName.setText(std.getStdName());
+		tfKor.setText(std.getKor()+"");
+		tfMath.setText(std.getKor()+"");
+		tfEng.setText(std.getEng()+"");
+	}
+	
+	public void clearTf() {
+		tfStdNo.setText("");
+		tfStdName.setText("");
+		tfKor.setText("");
+		tfMath.setText("");
+		tfEng.setText("");
+	}
+}
